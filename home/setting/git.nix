8f16865d29ac48ca2a600b912f9965df71a4ff31
Kaelwen <alyria83@gmail.com>
2026-02-11 18:48:26 +0800

    放弃gitee，转github

--- home/setting/git.nix
+++ home/setting/git.nix
@@ -4,11 +4,9 @@
     enable = true;
     settings = {
       user = {
-        name = "binigo";
+        name = "Kaelwen";
         email = "alyria83@gmail.com";
       };
-      # http.proxy = "http://127.0.0.1:7897";
-      # https.proxy = "http://127.0.0.1:7897";
     };
   };
 }
